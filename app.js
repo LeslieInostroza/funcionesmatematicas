854cc74f8cf10beae55ff2a533cc80761354310e
Leslie <leslie.ino@gmail.com>
2018-05-27 21:50:52 -0400

    ecuacion matematica, dolorosa de cabeza

---
+++ app.js
@@ -0,0 +1,9 @@
+const add = function (x, y) {
+    return x + y;
+ }
+
+ const multiply = function (x, y) {
+    return x * y;
+ }
+resultado = multiply(36325, add(9824, 777));{
+console.log(resultado);}
